dc3ab2f1d78f754a196699d75af6882d44a1a7a7
admls <adam.the.sherwood@gmail.com>
2019-04-28 00:08:48 +0100

    init middle and right

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -9,17 +9,26 @@ export class Navbar {
   @Element() el: HTMLElement;
 
   @State() showLeft = false;
+  @State() showMiddle = false;
+  @State() showRight = false;
 
   @Prop() styles;
   @Prop({ reflectToAttr: true }) background: string = "black";
   @Prop({ reflectToAttr: true }) color: string = "white";
-  @Prop({ reflectToAttr: true }) height: string = "50px";
+  @Prop({ reflectToAttr: true }) height: string = "";
   @Prop({ reflectToAttr: true }) position: string = "fixed";
   @Prop({ reflectToAttr: true }) display: string = "block";
 
   @Listen("click")
   onclick(e) {
-    console.log(e.target);
+    let elmnt = e.target;
+    while (elmnt && !elmnt.matches("admls-navbar")) {
+      if (elmnt.matches('[slot="left-icon"]')) {
+        this.showLeft = !this.showLeft;
+        break;
+      }
+      elmnt = elmnt.parentElement;
+    }
   }
 
   render() {
@@ -30,16 +39,39 @@ export class Navbar {
       position: this.position,
       width: "100%",
       top: "0",
-      left: "0"
+      left: "0",
+      padding: ".8em"
     };
     return (
-      <nav style={navStyle}>
+      <nav id="nav" style={navStyle}>
         <div id="left-wrapper">
           <slot name="left-icon" />
-          <div id="left-list">
+          <div
+            id="left-list"
+            class={this.showLeft ? "hidden-list" : "shown-list"}
+          >
             <slot name="left" />
           </div>
         </div>
+        <div id="middle-wrapper">
+          <slot name="middle-icon" />
+          <div
+            id="middle-list"
+            class={this.showMiddle ? "hidden-list" : "shown-list"}
+          >
+            <slot name="middle" />
+          </div>
+        </div>
+        <div id="right-wrapper">
+          <slot name="right-icon" />
+          <div
+            id="right-list"
+            class={this.showRight ? "hidden-list" : "shown-list"}
+          >
+            <slot name="right" />
+          </div>
+        </div>
+
         {/* <slot name="middle" />
         <slot name="middle-icon" />
         <slot name="right" />
